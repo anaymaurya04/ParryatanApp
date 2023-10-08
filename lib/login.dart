2d2870a378717240d9d2a7fcb11d2585a3ee88e8
itsmezain <zainaliald8282@gmail.com>
2023-10-09 00:06:16 +0530

    Skip_Button_FIX

--- lib/login.dart
+++ lib/login.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:test_scanner/main.dart';
 import 'package:material/material.dart';
 import 'package:test_scanner/color/color.dart';
 
+
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -15,136 +17,135 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: gblack,
-      body: Stack(children: [
-        Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
+      body: Stack(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 26.0, horizontal: 0.0),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Opacity(
+                opacity: 0.5,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const MyHomePage();
+                    }));
+                  },
+                  child: const Text(
+                    'Skip',
+                    style: TextStyle(
+                      color: gskin,
+                      fontSize: 12,
+                      fontFamily: "Nexa-Trial-Regular",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   mainAxisSize: MainAxisSize.max,
                   children: [
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      child: Opacity(
-                          opacity: 0.5,
-                          child: TextButton(
-                            onPressed: () {},
-                            child: const Text(
-                              'Skip',
-                              style: TextStyle(
-                                  color: gskin,
-                                  fontSize: 12,
-                                  fontFamily: "Nexa-Trial-Regular"),
-                            ),
-                          )),
-                    ),
                     const Text(
                       'Login',
                       style: TextStyle(
-                          color: gskin,
-                          fontFamily: "Nexa-Trial-Regular",
-                          fontSize: 30),
+                        color: gskin,
+                        fontFamily: "Nexa-Trial-Regular",
+                        fontSize: 30,
+                      ),
                     ),
                     const SizedBox(
                       height: 32,
                     ),
                     Form(
                       child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            TextFormField(
-                                style: const TextStyle(
-                                  // Customize the text style of the input
-                                  fontSize:
-                                      16, // Adjust the font size as needed
-                                  color: gskin, // Text color
-                                  fontFamily: "Nexa-Trial-Regular",
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: gskin,
+                              fontFamily: "Nexa-Trial-Regular",
+                            ),
+                            decoration: InputDecoration(
+                              labelText: 'Email Id',
+                              labelStyle: const TextStyle(color: Colors.grey),
+                              alignLabelWithHint: true,
+                              hintText: 'Enter your email',
+                              hintStyle: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
                                 ),
-                                decoration: InputDecoration(
-                                  // Customize the input decoration
-                                  labelText: 'Email Id',
-                                  labelStyle:
-                                      const TextStyle(color: Colors.grey),
-                                  alignLabelWithHint: true,
-                                  hintText:
-                                      'Enter your email', // Placeholder text
-                                  hintStyle: const TextStyle(
-                                    color:
-                                        Colors.grey, // Placeholder text color
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    // Border when the field is not focused
-                                    borderSide: const BorderSide(
-                                      color: Colors.grey,
-                                    ),
-                                    borderRadius: BorderRadius.circular(
-                                        8.0), // Border radius
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    // Border when the field is focused
-                                    borderSide: const BorderSide(
-                                      color: gskin, // Border color when focused
-                                    ),
-                                    borderRadius: BorderRadius.circular(
-                                        8.0), // Border radius
-                                  ),
-                                )),
-                            const Text(
-                              'Enter a valid email id',
-                              style: TextStyle(
-                                  color: Color.fromARGB(80, 243, 232, 228),
-                                  fontFamily: 'Nexa-Trial-Regular'),
-                            ),
-                            const SizedBox(
-                              height: 28,
-                            ),
-                            TextFormField(
-                                style: const TextStyle(
-                                  // Customize the text style of the input
-                                  fontSize:
-                                      16, // Adjust the font size as needed
-                                  color: gskin, // Text color
-                                  fontFamily: "Nexa-Trial-Regular",
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: gskin,
                                 ),
-                                decoration: InputDecoration(
-                                  // Customize the input decoration
-                                  labelText: 'Password',
-                                  labelStyle:
-                                      const TextStyle(color: Colors.grey),
-                                  alignLabelWithHint: true,
-                                  hintText:
-                                      'Enter your password', // Placeholder text
-                                  hintStyle: const TextStyle(
-                                    color:
-                                        Colors.grey, // Placeholder text color
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    // Border when the field is not focused
-                                    borderSide: const BorderSide(
-                                      color: Colors.grey,
-                                    ),
-                                    borderRadius: BorderRadius.circular(
-                                        8.0), // Border radius
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    // Border when the field is focused
-                                    borderSide: const BorderSide(
-                                      color: gskin, // Border color when focused
-                                    ),
-                                    borderRadius: BorderRadius.circular(
-                                        8.0), // Border radius
-                                  ),
-                                )),
-                            const Text(
-                              'Incorrect Password',
-                              style: TextStyle(
-                                  color: Color.fromARGB(80, 243, 232, 228),
-                                  fontFamily: 'Nexa-Trial-Regular'),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
                             ),
-                          ]),
+                          ),
+                          const Text(
+                            'Enter a valid email id',
+                            style: TextStyle(
+                                color: Color.fromARGB(80, 243, 232, 228),
+                                fontFamily: 'Nexa-Trial-Regular'),
+                          ),
+                          const SizedBox(
+                            height: 28,
+                          ),
+                          TextFormField(
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: gskin,
+                              fontFamily: "Nexa-Trial-Regular",
+                            ),
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: const TextStyle(color: Colors.grey),
+                              alignLabelWithHint: true,
+                              hintText: 'Enter your password',
+                              hintStyle: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.grey,
+                                ),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: gskin,
+                                ),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10, // Adjusted height for better spacing
+                          ),
+                          const Text(
+                            'Incorrect Password',
+                            style: TextStyle(
+                              color: Color.fromARGB(80, 243, 232, 228),
+                              fontFamily: 'Nexa-Trial-Regular',
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                     const SizedBox(
                       height: 50,
@@ -154,14 +155,16 @@ class _LoginPageState extends State<LoginPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         SizedBox(
-                            height: 30,
-                            child: Image.asset('lib/images/search.png')),
+                          height: 30,
+                          child: Image.asset('lib/images/search.png'),
+                        ),
                         const SizedBox(
                           width: 20,
                         ),
                         SizedBox(
-                            height: 30,
-                            child: Image.asset('lib/images/facebook.png')),
+                          height: 30,
+                          child: Image.asset('lib/images/facebook.png'),
+                        ),
                       ],
                     ),
                     const SizedBox(
@@ -170,27 +173,24 @@ class _LoginPageState extends State<LoginPage> {
                     Container(
                       height: 35,
                       decoration: BoxDecoration(
-                        color: gpurple, // Button background color
-                        borderRadius:
-                            BorderRadius.circular(13.0), // Button border radius
+                        color: gpurple,
+                        borderRadius: BorderRadius.circular(13.0),
                         boxShadow: const [
                           BoxShadow(
-                            color: Colors.black26, // Shadow color
-                            blurRadius: 0, // Blur radius (adjust as needed)
-                            offset: Offset(
-                                4, 4), // Offset (adjust for vertical position)
+                            color: Colors.black26,
+                            blurRadius: 0,
+                            offset: Offset(4, 4),
                           ),
                         ],
                       ),
                       child: ElevatedButton(
                         onPressed: () {},
                         style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              Colors.transparent, // Make the button transparent
-                          elevation: 0, // Remove button elevation
+                          primary: Colors.transparent,
+                          elevation: 0,
                           padding: const EdgeInsets.symmetric(
                             horizontal: 25.0,
-                          ), // Adjust the padding as needed
+                          ),
                         ),
                         child: const Text(
                           "Login",
@@ -201,23 +201,30 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ),
                     ),
+                    const SizedBox(
+                      height: 10, // Adjusted height for better spacing
+                    ),
                     Opacity(
-                        opacity: 0.5,
-                        child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'New User? Register',
-                            style: TextStyle(
-                                color: gskin,
-                                fontSize: 12,
-                                fontFamily: "Nexa-Trial-Regular"),
+                      opacity: 0.5,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'New User? Register',
+                          style: TextStyle(
+                            color: gskin,
+                            fontSize: 12,
+                            fontFamily: "Nexa-Trial-Regular",
                           ),
-                        )),
-                  ]),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
