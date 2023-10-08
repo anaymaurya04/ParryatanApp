c51bed55887e02a116698017c7dac03276adbbc0
anaymaurya04 <anaymaurya04@gmail.com>
2023-10-08 14:52:57 +0530

    notworking

--- lib/login.dart
+++ lib/login.dart
@@ -6,40 +6,8 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return  const Scaffold(
         backgroundColor: gblack,
-        body: SafeArea(
-            child: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-              const SizedBox(height: 50),
-              const Flexible(flex:2,child: Text(
-                  'Login',
-                  style: TextStyle(
-                      fontFamily: "Nexa-Trial-Regular",
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 247, 235, 235),
-                      fontSize: 35),
-                )),
-                const SizedBox(height: 10,width: double.infinity,),
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 15),
-                    child: Container(decoration: BoxDecoration(color: Colors.grey),),
-                  ),
-                )),
-              Flexible(child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 15),
-                    child: Container(decoration: BoxDecoration(color: Colors.grey),),
-                  ),
-                )),
-              Flexible(child: Container(color: Colors.brown,)),
-              Flexible(child: Container(color: Colors.grey,))
-            ]))));
+        );
   }
 }
