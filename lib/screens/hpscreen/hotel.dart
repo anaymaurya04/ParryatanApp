03371d866851d7eac0eacac2b1105bf791eae7c0
itsmezain <zainaliald8282@gmail.com>
2023-10-14 11:58:53 +0530

    Tiles

--- lib/screens/hpscreen/hotel.dart
+++ lib/screens/hpscreen/hotel.dart
@@ -27,6 +27,57 @@ class Hotel extends StatelessWidget {
         ),
         elevation: 0.0,
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: 800,
+          width: double.infinity,
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              TappableRoundedListItem('lib/images/Kanha1.png', () {}),
+              const SizedBox(height: 15),
+              TappableRoundedListItem('lib/images/HotelPrayag.png', () {
+                // Add your action here when the second item is tapped.
+              }),
+              const SizedBox(height: 15),
+              TappableRoundedListItem('lib/images/GC.png', () {
+                // Add your action here when the third item is tapped.
+              }),
+              const SizedBox(height: 15),
+              TappableRoundedListItem('lib/images/Yartrik.png', () {
+                // Add your action here when the fourth item is tapped.
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TappableRoundedListItem extends StatelessWidget {
+  final String imagePath;
+  final VoidCallback onTap;
+
+  TappableRoundedListItem(this.imagePath, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: glight,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(25),
+          child: Image.asset(
+            imagePath,
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
     );
   }
 }
